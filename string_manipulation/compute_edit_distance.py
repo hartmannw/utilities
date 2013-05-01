00a8b313ae8c4a4fe6dc04de1806c9339985e122
hartmanw <hartmannw@gmail.com>
2013-05-01 09:56:59 +0200

    Fixed Edit Distance Computation

--- string_manipulation/compute_edit_distance.py
+++ string_manipulation/compute_edit_distance.py
@@ -4,7 +4,8 @@
 # This is free and unencumbered software released into the public domain.
 # See the UNLICENSE file for more information.
 #
-# Computes the edit distance between two strings.
+# Computes the edit distance between two strings and provides the
+# transformations necessary to transform the target into the guess.
 
 import getopt, sys, string, re, operator
 
@@ -23,7 +24,7 @@ def main():
     if o in ("-g", "--guess"):
       guess= str(a)
 
-  print guess + " --> " + target
+  print target + " --> " + guess
   target = re.split(' ', target)
   guess = re.split(' ', guess)
 
@@ -34,48 +35,46 @@ def main():
 def EditDistance(target, guess):
 ## Rewrite going from 0 to len(target), I need the extra row and column
 ## to keep track of transformations
-  d = [[ 0 for i in range(len(target))] for j in range(len(guess))]
-  transform = [[ {} for i in range(len(target))] for j in range(len(guess))]
-  for i in range(len(guess)):
-    for j in range(len(target)):
-      if guess[i] == target[j]:
-        d[i][j] = GetValue(d, i-1, j-1)
-        transform[i][j] = GetTransform(transform, i-1, j-1).copy()
+  n = len(guess) + 1
+  m = len(target) + 1
+  d = [[ 0 for i in range(m)] for j in range(n)]
+  transform = [[ {} for i in range(m)] for j in range(n)]
+
+  for i in range(1,n):
+    d[i][0]=i
+    transform[i][0] = transform[i-1][0].copy()
+    transform[i][0][str(i-2) + ":" + str(i-1)] = guess[i-1]
+  
+  for j in range(1,m):
+    d[0][j]=j
+    transform[0][j] = transform[0][j-1].copy()
+    transform[0][j][str(j-1)] = ""
+  
+  for i in range(1,n):
+    for j in range(1,m):
+      if guess[i-1] == target[j-1]:
+        d[i][j] = d[i-1][j-1]
+        transform[i][j] = transform[i-1][j-1].copy()
       else:
-        deletion = GetValue(d, i, j-1) + 1
-        insertion = GetValue(d, i-1, j) + 1
-        substitution = GetValue(d, i-1, j-1) + 1
+        deletion = d[i][j-1]+ 1
+        insertion = d[i-1][j]+ 1
+        substitution = d[i-1][j-1]+ 1
         values = [substitution, deletion, insertion]
         minindex, minvalue = min(enumerate(values), key=operator.itemgetter(1))
         d[i][j] = minvalue
         if minindex == 0:
-          transform[i][j] = GetTransform(transform, i-1, j-1).copy()
-          transform[i][j][str(i)] = target[j]
+          transform[i][j] = transform[i-1][j-1].copy()
+          transform[i][j][str(i-1)] = guess[j-1]
         elif minindex == 1:
-          transform[i][j] = GetTransform(transform, i, j-1).copy()
-          transform[i][j][str(i)] = ""
+          transform[i][j] = transform[i][j-1].copy()
+          transform[i][j][str(j-1)] = ""
         else:
-          transform[i][j] = GetTransform(transform, i-1, j).copy()
-          transform[i][j][str(i) + ":" + str(i+1)] = target[j]
+          transform[i][j] = transform[i-1][j].copy()
+          transform[i][j][str(i-2) + ":" + str(i-1)] = guess[i-1]
 
   print d
   print transform
-  return d[len(guess)-1][len(target)-1], transform[len(guess)-1][len(target)-1]
-
-
-def GetValue(d, i, j):
-  if min(i,j) < -1:
-    return int("inf")
-  if min(i,j) < 0:
-    return max(i,j) + 1
-  return d[i][j]
-
-def GetTransform(t, i, j):
-  if i < 0:
-    return GetTransform(t, 0, j)
-  if j < 0:
-    return GetTransform(t,i,0)
-  return t[i][j]
+  return d[n-1][m-1], transform[n-1][m-1]
 
 def usage():
   print ' --help Prints this message'
