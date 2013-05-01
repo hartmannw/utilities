fe4de560dae2625c080354c980f04807118a9400
hartmanw <hartmannw@gmail.com>
2013-05-01 10:35:40 +0200

    Moved the edit distance function into a module.
    The string_manip module should now hold all general string manipulation
    functions.

--- string_manipulation/compute_edit_distance.py
+++ string_manipulation/compute_edit_distance.py
@@ -7,7 +7,7 @@
 # Computes the edit distance between two strings and provides the
 # transformations necessary to transform the target into the guess.
 
-import getopt, sys, string, re, operator
+import getopt, sys, string, re, operator, string_manip
 
 def main():
   try:
@@ -28,53 +28,10 @@ def main():
   target = re.split(' ', target)
   guess = re.split(' ', guess)
 
-  distance, transformations = EditDistance(target, guess)
+  distance, transformations = string_manip.EditDistance(target, guess)
   print distance
   print transformations
 
-def EditDistance(target, guess):
-## Rewrite going from 0 to len(target), I need the extra row and column
-## to keep track of transformations
-  n = len(guess) + 1
-  m = len(target) + 1
-  d = [[ 0 for i in range(m)] for j in range(n)]
-  transform = [[ {} for i in range(m)] for j in range(n)]
-
-  for i in range(1,n):
-    d[i][0]=i
-    transform[i][0] = transform[i-1][0].copy()
-    transform[i][0][str(i-2) + ":" + str(i-1)] = guess[i-1]
-  
-  for j in range(1,m):
-    d[0][j]=j
-    transform[0][j] = transform[0][j-1].copy()
-    transform[0][j][str(j-1)] = ""
-  
-  for i in range(1,n):
-    for j in range(1,m):
-      if guess[i-1] == target[j-1]:
-        d[i][j] = d[i-1][j-1]
-        transform[i][j] = transform[i-1][j-1].copy()
-      else:
-        deletion = d[i][j-1]+ 1
-        insertion = d[i-1][j]+ 1
-        substitution = d[i-1][j-1]+ 1
-        values = [substitution, deletion, insertion]
-        minindex, minvalue = min(enumerate(values), key=operator.itemgetter(1))
-        d[i][j] = minvalue
-        if minindex == 0:
-          transform[i][j] = transform[i-1][j-1].copy()
-          transform[i][j][str(i-1)] = guess[j-1]
-        elif minindex == 1:
-          transform[i][j] = transform[i][j-1].copy()
-          transform[i][j][str(j-1)] = ""
-        else:
-          transform[i][j] = transform[i-1][j].copy()
-          transform[i][j][str(i-2) + ":" + str(i-1)] = guess[i-1]
-
-  print d
-  print transform
-  return d[n-1][m-1], transform[n-1][m-1]
 
 def usage():
   print ' --help Prints this message'
---
+++ string_manipulation/string_manip.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+#
+# William Hartmann (hartmannw@gmail.com)
+# This is free and unencumbered software released into the public domain.
+# See the UNLICENSE file for more information.
+#
+# Contains various string manipulation functions.
+
+import operator
+
+# Computes both the edit distance between the two strings, but also the
+# necessary steps to transform the target into the guess. Due to the way the
+# operations are stored, it only allows one string operation per position. If
+# the strings are significantly different in terms of length, then the
+# transformations may be incorrect.
+def EditDistance(target, guess):
+  n = len(guess) + 1
+  m = len(target) + 1
+  d = [[ 0 for i in range(m)] for j in range(n)]
+  transform = [[ {} for i in range(m)] for j in range(n)]
+
+  for i in range(1,n):
+    d[i][0]=i
+    transform[i][0] = transform[i-1][0].copy()
+    transform[i][0][str(i-2) + ":" + str(i-1)] = guess[i-1]
+  
+  for j in range(1,m):
+    d[0][j]=j
+    transform[0][j] = transform[0][j-1].copy()
+    transform[0][j][str(j-1)] = ""
+  
+  for i in range(1,n):
+    for j in range(1,m):
+      if guess[i-1] == target[j-1]:
+        d[i][j] = d[i-1][j-1]
+        transform[i][j] = transform[i-1][j-1].copy()
+      else:
+        deletion = d[i][j-1]+ 1
+        insertion = d[i-1][j]+ 1
+        substitution = d[i-1][j-1]+ 1
+        values = [substitution, deletion, insertion]
+        minindex, minvalue = min(enumerate(values), key=operator.itemgetter(1))
+        d[i][j] = minvalue
+        if minindex == 0:
+          transform[i][j] = transform[i-1][j-1].copy()
+          transform[i][j][str(i-1)] = guess[j-1]
+        elif minindex == 1:
+          transform[i][j] = transform[i][j-1].copy()
+          transform[i][j][str(j-1)] = ""
+        else:
+          transform[i][j] = transform[i-1][j].copy()
+          transform[i][j][str(i-2) + ":" + str(i-1)] = guess[i-1]
+
+  return d[n-1][m-1], transform[n-1][m-1]
